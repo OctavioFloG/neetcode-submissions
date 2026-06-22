3a03741799d2ab6358e1297664ecdd25e2f9043d
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2026-06-22 09:40:05 -0600

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,16 @@
+// Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if (map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
+            }
+
+            // Construir el map mientras iteramos, para evitar que regrese el mismo índice
+            // Si no existe el número en el map entonces lo añadimos
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
